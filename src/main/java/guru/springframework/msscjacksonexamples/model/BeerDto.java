c45ae5f5e640e405f41cf902bc14127bafc43633
topgunjoe <joe.w.wachiuri@gmail.com>
2023-02-01 07:10:02 +0300

    Altering date information - Jackson

--- src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
+++ src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
@@ -39,8 +39,9 @@ public class BeerDto {
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
 
-    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastUpdatedDate;
 }
